367f11760d525cb8e402a682c6f088c2836fdf0f
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-11 16:46:30 +0530

    ADDED : Display the board and Select the cell

--- Tictactoe.sh
+++ Tictactoe.sh
@@ -7,27 +7,75 @@ declare -a gameBoard
 gameBoard=(_ _ _ _ _ _ _ _ _)
 
 
-<<<<<<< HEAD
-case $rno in
-	0)
-		player=o ;;
-	1)
-		player=x ;;
-esac
-=======
 function toss() {
 
 	rno=$((RANDOM%2))
 
 	case $rno in
 		0)
-			player=o
-			computer=x ;;
+			player1=o
+			player2=x ;;
 		1)
-			player=x
-			computer=o ;;
+			player1=x
+			player2=o ;;
 	esac
-	echo $player $computer
+	echo $player1 $player2
 }
-toss
->>>>>>> UC3_Check_Player_First
+#toss
+
+function printBoard() {
+
+	row=0
+	count=1
+	for ((i=1;i<=3;i++))
+	do
+		echo "$((count++))| ${gameBoard[$row]} | $((count++))| ${gameBoard[$((row=$row+1))]} | $((count++))| ${gameBoard[$((row=$row+1))]} | "
+		row=$(($row+1))
+	done
+}
+#printBoard
+
+function changeTurn() {
+	if [ $1 == x ]
+	then
+		changeTurn=o
+	else
+		changeTurn=x
+	fi
+}
+
+function playerMove () {
+	flag=ture
+	while [[ $flag == ture ]]
+	do
+		read -p "Enter the position 1 to 9 " pos
+
+		if [[ $pos -le 9 && ${gameBoard[$(($pos-1))]}!=o && ${gameBoaed[$(($pos-1))]}!=x ]]
+		then
+			gameBoard[$((pos-1))]=$1
+			flag=false
+		else
+			echo "invalid Postion"
+		fi
+	done
+	changeTurn $1
+}
+
+function main () {
+	printBoard
+	read play1 play2 < <( toss )
+echo  $play1 $play2
+echo "Main function"
+
+	for ((i=1; i<=9; i++))
+	do
+		if [[ $play1 == $changeTurn ]]
+		then
+				playerMove $play1
+		else
+				playerMove $play2
+		fi
+				printBoard
+	done
+}
+main
