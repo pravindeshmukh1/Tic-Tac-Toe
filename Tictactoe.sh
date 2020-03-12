9cb01c32adc5e53079b0939401e2379d011e88cb
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-12 12:03:27 +0530

    ADDED : Blocked Opponent

--- Tictactoe.sh
+++ Tictactoe.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -x
 
-echo "Welcome to the Tic Tac Toe"
+echo "Welcome to the Tic Tac Toe "
 
 declare -a gameBoard
 changeTurn=x
+
 gameBoard=(_ _ _ _ _ _ _ _ _)
 
+mainFlag=false
 
-function toss() {
 
-	rno=$((RANDOM%2))
+function toss(){
 
-	case $rno in
+	toss=$((RANDOM%2))
+
+	case $toss in
+
 		0)
 			player=o
 			computer=x ;;
@@ -19,23 +23,12 @@ function toss() {
 			player=x
 			computer=o ;;
 	esac
+
 	echo $player $computer
 }
 
-
-function printBoard() {
-
-	row=0
-	count=1
-	for ((i=1;i<=3;i++))
-	do
-		echo "$((count++))| ${gameBoard[$row]} | $((count++))| ${gameBoard[$((row=$row+1))]} | $((count++))| ${gameBoard[$((row=$row+1))]} | "
-		row=$(($row+1))
-	done
-}
-
-
 function changeTurn() {
+
 	if [[ $1 == x ]]
 	then
 		changeTurn=o
@@ -44,26 +37,36 @@ function changeTurn() {
 	fi
 }
 
-function playerMove () {
+function printBoard() {
+
+	row=0
+	count=1
+	for (( i=0;i<3;i++ ))
+	do
+		echo " $((count++))| ${gameBoard[$row]} | $((count++))| ${gameBoard[$((row=$row+1))]} | $((count++))| ${gameBoard[$((row=$row+1))]} |"
+		row=$(($row+1))
+	done
+}
+
+function playerMove(){
 
 	flag=true
-
 	while [[ $flag == true ]]
 	do
-		read -p "Enter the position 1 to 9 : " pos
+		read -p "Enter the Position $1 " pos
 
-		if (( $pos <= 9 && ${gameBoard[$pos-1]}!=x && ${gameBoard[$pos-1]}!=o ))
+		if (( $pos <= 9 && ${gameBoard[$pos-1]}!=x &&  ${gameBoard[$pos-1]}!=o ))
 		then
 			gameBoard[$(($pos - 1))]=$1
 			flag=false
 		else
-			echo "invalid Postion"
+			echo "invalide Position"
 		fi
 	done
-	changeTurn $1
+		changeTurn $1
 }
 
-function computerMove() {
+function computerMove(){
 
 	computerPos=$(( RANDOM%9 ))
 
@@ -76,79 +79,260 @@ function computerMove() {
 		changeTurn $1
 }
 
-function checkWin() {
+function rowBlocker(){
 
-row=0
-col=0
-diag1=0
-diag2=2
-flag=false
+	if [[ $1 == x ]]
+	then
+		checkForComp=o
+	else
+		checkForComp=x
+	fi
 
-	for ((i=0; i<3;i++))
+	flag=true
+	for (( i=0;i<9;i+=3 ))
 	do
-		if [[ ${gameBoard[$row]} == $1 && ${gameBoard[$((row=$row+1))]} == $1  && ${gameBoard[$((row=$row+1))]} == $1 ]]
+		if [[ ${gameBoard[$i]} == $1 && ${gameBoard[$i+1]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+2]} != $1 && ${gameBoard[$i+2]} != $2 && ${gameBoard[$i+2]} != $checkForComp ]]
+			then
+				gameBoard[$i+2]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i]} == $1 && ${gameBoard[$i+2]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+1]} != $1 && ${gameBoard[$i+1]} != $2 && ${gameBoard[$i+1]} != $checkForComp ]]
+			then
+				gameBoard[$i+1]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i+1]} == $1 && ${gameBoard[$i+2]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i]} != $1 && ${gameBoard[$i]} != $2 && ${gameBoard[$i]} != $checkForComp ]]
+			then
+				gameBoard[$i]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		fi
+	done
+
+		if [[ $flag == true ]]
+		then
+			columnBlocker $1 $2 $checkForComp
+		fi
+		changeTurn $2
+}
+
+function columnBlocker(){
+
+	flag=true
+	for (( i=0;i<3;i++ ))
+	do
+		if [[ ${gameBoard[$i]} == $1 && ${gameBoard[$i+3]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+6]} != $1 && ${gameBoard[$i+6]} != $2 && ${gameBoard[$i+6]} != $3 ]]
+			then
+				gameBoard[$i+6]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i]} == $1 && ${gameBoard[$i+6]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+3]} != $1 && ${gameBoard[$i+3]} != $2 && ${gameBoard[$i+3]} != $3 ]]
+			then
+				gameBoard[$i+3]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i+3]} == $1 && ${gameBoard[$i+6]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i]} != $1 && ${gameBoard[$i]} != $2 && ${gameBoard[$i]} != $3 ]]
+			then
+				gameBoard[$i]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		fi
+	done
+
+	if [[ $flag == true ]]
+	then
+		diag1Blocker $1 $2 $3
+	fi
+	changeTurn $2
+}
+
+function diag1Blocker(){
+
+	flag=true
+	for (( i=0;i<9;i+=3 ))
+	do
+		if [[ ${gameBoard[$i]} == $1 && ${gameBoard[$i+4]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+8]} != $1 && ${gameBoard[$i+8]} != $2 && ${gameBoard[$i+8]} != $3 ]]
+			then
+				gameBoard[$i+8]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i]} == $1 && ${gameBoard[$i+8]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+4]} != $1 && ${gameBoard[$i+4]} != $2 && ${gameBoard[$i+4]} != $3 ]]
+			then
+				gameBoard[$i+4]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i+4]} == $1 && ${gameBoard[$i+8]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i]} != $1 && ${gameBoard[$i]} != $2 && ${gameBoard[$i]} != $3 ]]
+			then
+				gameBoard[$i]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		fi
+	done
+
+	if [[ $flag == true ]]
+	then
+		diag2Blocker $1 $2 $3
+	fi
+		changeTurn $2
+}
+
+
+function diag2Blocker(){
+
+	flag=true
+	for (( i=0;i<9;i+=3 ))
+	do
+		if [[ ${gameBoard[$i+2]} == $1 && ${gameBoard[$i+4]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+6]} != $1 && ${gameBoard[$i+6]} != $2 && ${gameBoard[$i+6]} != $3 ]]
+			then
+				gameBoard[$i+6]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i+2]} == $1 && ${gameBoard[$i+6]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+4]} != $1 && ${gameBoard[$i+4]} != $2 && ${gameBoard[$i+4]} != $3 ]]
+			then
+				gameBoard[$i+4]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		elif [[ ${gameBoard[$i+4]} == $1 && ${gameBoard[$i+6]} == $1 ]]
+		then
+			if [[ ${gameBoard[$i+2]} != $1 && ${gameBoard[$i+2]} != $2 && ${gameBoard[$i+2]} != $3 ]]
+			then
+				gameBoard[$i+2]=$2
+				mainFlag=true
+				flag=false
+				break
+			fi
+		fi
+	done
+
+		changeTurn $2
+}
+
+function checkWin(){
+
+	row=0
+	col=0
+	diag1=0
+	diag2=2
+	flag=false
+
+	for (( i=0;i<3;i++ ))
+	do
+		if [[ ${gameBoard[$row]} == $1 && ${gameBoard[$((row=$row+1))]} == $1 && ${gameBoard[$((row=$row+1))]} == $1 ]]
 		then
 			flag=true
 			break
 		fi
+
 		if [[ ${gameBoard[$col]} == $1 && ${gameBoard[$((col=$col+3))]} == $1 && ${gameBoard[$((col=$col+3))]} == $1 ]]
 		then
 			flag=true
 			break
 		fi
+
 		if [[ ${gameBoard[$diag1]} == $1 && ${gameBoard[$((diag1=$diag1+4))]} == $1 && ${gameBoard[$((diag1=$diag1+4))]} == $1 ]]
 		then
 			flag=true
 			break
 		fi
+
 		if [[ ${gameBoard[$diag2]} == $1 && ${gameBoard[$((diag2=$diag2+2))]} == $1 && ${gameBoard[$((diag2=$diag2+2))]} == $1 ]]
 		then
 			flag=true
 			break
 		fi
-		row=$(( ($i+1) * 3 ))
-		col=$(( $i+1 ))
+		row=$(( ($i + 1) * 3 ))
+		col=$(( $i + 1 ))
 	done
-	echo $flag
+		echo $flag
 }
 
-function main() {
-	printBoard
-	status=flag
+function main(){
 
+	status=false
+	printBoard
 	read player computer < <( toss )
-	echo " player $player computer $computer "
+
+	echo "Player $player Computer $computer"
 	echo "Main function"
 
-	for (( i=0;i<${#gameBoard[@]};i++ ))
+	for (( j=0;j<${#gameBoard[@]};j++ ))
 	do
 		if [[ $player == $changeTurn ]]
 		then
-				playerMove $player
-				printBoard
-				status=$( checkWin $player )
-				if	[[ $status == true ]]
-				then
-					echo "Player 1 win"
+			playerMove  $player
+			status=$( checkWin $player )
+			if [[ $status == true ]]
+			then
+					echo "Player Win"
 					break
-				fi
+			fi
 		else
-			echo "Computer Turn "
-			 computerMove $computer
-				printBoard
-				status=$( checkWin $computer )
-				if	[[ $status == true ]]
-				then
-					echo "Computer win"
+			rowBlocker $computer $computer
+			status=$( checkWin $computer )
+			if [[ $status == true ]]
+			then
+					printBoard
+					echo "Computer Win"
 					break
-				fi
+			else
+					rowBlocker $player $computer
+			fi
 
+			if [[ $mainFlag == false ]]
+			then
+				computerMove $computer
+			fi
+				printBoard
 		fi
-			#printBoard
 	done
+
 	if [[ $status == false ]]
 	then
-	echo "Game Tie"
+		echo "Game Tie.."
 	fi
 }
 main
